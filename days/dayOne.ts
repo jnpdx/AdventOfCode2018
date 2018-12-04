4397e4920a755c16e3c3ffea77529e1bfda7e2ec
jnpdx <john@johnnastos.com>
2018-12-04 12:33:43 -0800

    Finished day 1

---
+++ days/dayOne.ts
@@ -0,0 +1,26 @@
+import { dayOneInput } from "./dayOneInput";
+
+export function dayOnePart1() {
+  return dayOneInput.reduce(
+    (totalFreq, currentFrequency) => totalFreq + currentFrequency,
+    0
+  );
+}
+
+export function dayOnePart2() {
+  let frequencies = new Set<number>();
+  let frequency = 0;
+  let index = 0;
+
+  while (true) {
+    const currentFrequency = dayOneInput[index];
+    frequency += currentFrequency;
+    if (frequencies.has(frequency)) {
+      return frequency;
+    } else {
+      frequencies.add(frequency);
+      index++;
+      if (index == dayOneInput.length) index = 0;
+    }
+  }
+}
---
+++ days/runner.ts
@@ -0,0 +1,4 @@
+import { dayOnePart1, dayOnePart2 } from "./dayOne";
+
+console.log("Part 1: " + dayOnePart1());
+console.log("Part 2: " + dayOnePart2());
