9140c70ada701454735bf6018dd8cc8668e9bcb3
jnpdx <john@johnnastos.com>
2018-12-04 15:00:17 -0800

    Getting input for day four in order

---
+++ days/dayFour.ts
@@ -0,0 +1,5 @@
+import { dayFourInput } from "./inputs/dayFourInput";
+
+export function dayFourPart1() {
+  console.log(dayFourInput);
+}
--- days/runner.ts
+++ days/runner.ts
@@ -1,6 +1,7 @@
 import { dayOnePart1, dayOnePart2 } from "./dayOne";
 import { dayTwoPart1, dayTwoPart2 } from "./dayTwo";
 import { dayThreePart1, dayThreePart2 } from "./dayThree";
+import { dayFourPart1 } from "./dayFour";
 
 // console.log("Day 1:");
 // console.log("Part 1: " + dayOnePart1());
@@ -8,6 +9,9 @@ import { dayThreePart1, dayThreePart2 } from "./dayThree";
 // console.log("Day 2:");
 // console.log("Part 1: " + dayTwoPart1());
 // console.log("Part 2: " + dayTwoPart2());
-console.log("Day 3:");
-console.log("Part 1: " + dayThreePart1());
-console.log("Part 2: " + dayThreePart2());
+// console.log("Day 3:");
+// console.log("Part 1: " + dayThreePart1());
+// console.log("Part 2: " + dayThreePart2());
+console.log("Day 4:");
+console.log("Part 1: " + dayFourPart1());
+// console.log("Part 2: " + dayThreePart2());
