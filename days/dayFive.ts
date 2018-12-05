1875c9249320cb661f2e9312c690403eecee6e4c
jnpdx <john@johnnastos.com>
2018-12-05 00:32:31 -0800

    Update dayFive.ts

--- days/dayFive.ts
+++ days/dayFive.ts
@@ -1,24 +1,20 @@
-import { dayFiveInput } from "./inputs/dayFiveInput";
+import { dayFiveInput, dayFiveTestInput } from "./inputs/dayFiveInput";
 
-const alphabet = "abcdefghijklmnopqrstuvwxyz";
-const letterPairs: string[] = [];
+const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+//const letterPairs: string[] = [];
 
-for (let i = 0; i < alphabet.length; i++) {
-  const currentLetter = alphabet[i];
-  letterPairs.push(`${currentLetter}${currentLetter.toUpperCase()}`);
-  letterPairs.push(`${currentLetter.toUpperCase()}${currentLetter}`);
-}
+const letterPairs = alphabet
+  .map(letter => `${letter}${letter.toUpperCase()}`)
+  .concat(alphabet.map(letter => `${letter.toUpperCase()}${letter}`));
 
 export function dayFive() {
-  let currentString = dayFiveInput;
+  let currentString = dayFiveTestInput;
 
   for (let i = 0; i < currentString.length - 1; i++) {
     const testPair = currentString.slice(i, i + 2);
-    //console.log(testPair);
     if (letterPairs.indexOf(testPair) != -1) {
       currentString =
         currentString.substring(0, i) + currentString.substring(i + 2);
-      //console.log("Set to: " + currentString);
       i = -1;
     }
   }
